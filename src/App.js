04d804f46dbddfaac058be5c12d0c1a93b623bf5
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-04 15:21:56 +0545

    y

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import React from "react";
+import "./App.css";
+import CardList from "./components/card-list.component";
+import Search from "./components/search.component";
+import Title from "./components/title.component";
 
-import './App.css';
+class App extends React.Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    this.state = {
+      users: [],
+      searchFilter: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((user) => this.setState({ users: user }));
+  }
+
+  render() {
+    const { users, searchFilter } = this.state;
+
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(searchFilter.toLowerCase()),
+      
+  
+      
+    );
+
+    return (
+      <div className="App">
+        <Title>Hello Monsters</Title>
+
+        <input
+          type="search"
+          placeholder="Search..."
+          onChange={(e) => this.setState({ searchFilter: e.target.value })}
+         className="search"/>
+
+        <CardList datas={filteredUsers} />
+      </div>
+    );
+  }
 }
 
 export default App;
